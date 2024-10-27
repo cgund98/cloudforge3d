3d541fadf1f742d78b8ea128af5f4bc2b4c445ca
Callum Gundlach <gundlachcallum@gmail.com>
2024-10-26 17:32:55 -0700

    Increase message retention to two weeks

--- cloud-deploy/cloud_deploy/cloud_deploy_stack.py
+++ cloud-deploy/cloud_deploy/cloud_deploy_stack.py
@@ -27,6 +27,7 @@ class CloudDeployStack(Stack):
             queue_name="cf3d-task-updates.fifo",
             fifo=True,
             visibility_timeout=Duration.seconds(30),
+            retention_period=Duration.days(14)
         )
 
         vpc = ec2.Vpc(
